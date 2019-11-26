e1b8fec4a37418a1e08b761c9d3e2b2618e738bd
oofoh <oofohlol1@gmail.com>
2019-11-25 18:51:28 -0800

    Assignment complete

---
+++ 01-java-fundamentals/02-fundamentals/01-first-java-program/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("Jake");
+        System.out.println(20);
+        System.out.println("Yucca Valley");
+    }
+}
